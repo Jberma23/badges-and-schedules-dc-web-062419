245a8906461b625d5e130f6c8cb9cc8b0afea283
Jesse Berman <jesse.ben.berman@gmail.com>
2019-06-16 19:25:50 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,9 +3,15 @@ def badge_maker(name)
  "Hello, my name is #{name}."
 end
 
-attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
 def batch_badge_creator(array)
 array.collect do |name|
   "Hello, my name is #{name}."
 end
 end
+
+def assign_rooms(array)
+ room_assignments = []
+ array.index_with_each {|name, index| room_assignments.push "Hello, #{name}! You'll be in room #{index +1}!" }
+ room_assignments
+ end
