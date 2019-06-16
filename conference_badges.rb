b40385696019c4a205fdff4d56aa421462ff9e93
Jesse Berman <jesse.ben.berman@gmail.com>
2019-06-16 19:03:14 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,6 +4,10 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array)
+  counter = 0
 array.each do
   "Hello, my name is #{array}"
+  counter += 1
+break if
+counter counter == array.length
 end
