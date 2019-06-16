ed649e79f278a9fbdf5feb7d78c9b4893091cf5c
Jesse Berman <jesse.ben.berman@gmail.com>
2019-06-16 19:15:42 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,10 +5,9 @@ end
 
 attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 def batch_badge_creator(array)
-i = 0
-while i <= array.length
-  name = array[i]
-   "Hello, my name is #{name}."
-  i += 1
-end
+  badge = []
+  array.each do |person|
+    badge << "Hello, my name is #{person}."
+  end
+  return badge
 end
