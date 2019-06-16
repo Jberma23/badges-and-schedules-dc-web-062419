c6d36293bf0a5369cff2323ac20b561f1cdfcd57
Jesse Berman <jesse.ben.berman@gmail.com>
2019-06-16 19:05:15 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,11 +2,13 @@
 def badge_maker(name)
  "Hello, my name is #{name}."
 end
-
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 def batch_badge_creator(array)
   counter = 0
 array.each do
   "Hello, my name is #{array}"
   counter += 1
+  break if
+  array.length == counter
 end
 end
